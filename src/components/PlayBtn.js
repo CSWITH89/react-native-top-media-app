a104096070a35d0e41f7929011558e702c31dbb0
Chris Withall <chriswithall@Chriss-MacBook-Pro-2.local>
2021-07-22 14:50:37 +0200

    Icons added and play btn completed

--- src/components/PlayBtn.js
+++ src/components/PlayBtn.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {View, TouchableWithoutFeedback, StyleSheet} from 'react-native';
 import {SCREEN_WIDTH} from '../consts/Layout';
+import FAIcon from 'react-native-vector-icons/FontAwesome';
 
 const PlayBtn = ({press}) => {
   return (
     <TouchableWithoutFeedback onPress={press}>
-      <View style={styles.circle}></View>
+      <View style={styles.circle}>
+        <FAIcon size={SCREEN_WIDTH * 0.05} color="#fff" name="play" />
+      </View>
     </TouchableWithoutFeedback>
   );
 };
